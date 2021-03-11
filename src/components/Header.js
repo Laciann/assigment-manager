330433a30e0384bec2a5bbe51fe13f9f6cee04f9
Laciann <abhishek.acharya@apexcollege.edu.np>
2021-03-11 21:36:15 +0545

    button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = () => {
+const Header = ({title}) => {
   return (
-    <header>
-      <h1> Assignment Tracker </h1>
+    <header className='header'>
+      <h1> {title} </h1>
+      <Button color='green' text='Add' onClick={onClick}/>
     </header>
   );
 };
+
+Header.defaultProps = {
+  title: 'Assigment Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
 
 export default Header;
